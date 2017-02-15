ed617a4822d517fbe8b7b50d1adca5bb9126c902
kevinch <ola@kevinch.com>
2017-02-15 09:45:05 -0200

    configuer storm path

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-angular.module('app', []).controller('appController', function() {
-    var vm = this;
-    vm.title = 'hello from angular';
-});
+angular.module('app', ['stormpath','stormpath.templates'])
+    .controller('appController', function() {
+        var vm = this;
+        vm.title = 'hello from angular';
+    })
+    .config(function (STORMPATH_CONFIG) {
+
+    // Specify your Client API domain here:
+    STORMPATH_CONFIG.ENDPOINT_PREFIX = 'https://ready-stream.apps.stormpath.io';
+  });
